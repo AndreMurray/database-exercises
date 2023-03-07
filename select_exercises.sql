f66fb84e78be3354fcad3383740f8c7758840610
Andre Murray <slushymatis@gmail.com>
2023-03-07 10:29:31 -0600

    pushing mysql exercises

---
+++ select_exercises.sql
@@ -0,0 +1,22 @@
+USE zenith_test_db;
+SELECT *
+From albums
+WHERE artist =  'Pink Floyd';
+
+SELECT release_date FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre FROM albums
+WHERE name = 'Nevermind';
+
+SELECT name
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT name
+FROM albums
+WHERE sales < 20;
+
+SELECT name
+FROM albums
+WHERE genre LIKE '%rock%';
---
+++ update_exercises.sql
@@ -0,0 +1,35 @@
+SELECT name AS 'All albums in the table'
+FROM albums;
+
+
+SELECT name AS 'albums released before 1980'
+FROM albums
+WHERE release_date < 1980;
+
+
+SELECT name AS 'albums by Michael Jackson'
+FROM albums
+WHERE artist = 'Michael Jackson';
+
+
+SELECT name
+FROM albums
+WHERE artist = 'Peter Jackson';
+
+
+UPDATE albums
+SET sales = sales * 10;
+
+SELECT * FROM albums;
+
+
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
+
+
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+
+SELECT * FROM albums;
